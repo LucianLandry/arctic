1b7f8405f65a5cd4e2900a1f171a835ce2a1d6fb
Lucian Landry <lucian_b_landry@yahoo.com>
2016-05-26 06:10:34 -0500

    CompStatsT -> ThinkerStatsT
    Made ThinkerStatsT be able to initialize itself.  Made comp.cpp:gStats
    static.

---
+++ ThinkerTypes.h
@@ -0,0 +1,49 @@
+//--------------------------------------------------------------------------
+//         ThinkerTypes.h - types used to communicate with the Thinker.
+//                           -------------------
+//  copyright            : (C) 2016 by Lucian Landry
+//  email                : lucian_b_landry@yahoo.com
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Library General Public License as
+//   published by the Free Software Foundation; either version 2 of the
+//   License, or (at your option) any later version.
+//
+//--------------------------------------------------------------------------
+
+#ifndef THINKERTYPES_H
+#define THINKERTYPES_H
+
+#include <string.h> // memset()
+
+// NOTE: these are not exact counts, since we do not want the speed hit that
+//  comes from updating these atomically.  We could have the child threads
+//  maintain their own stats while they are searching, but this still does not
+//  work for 'nodes' because the children need to quickly know when maxNodes
+//  has been met.
+struct ThinkerStatsT
+{
+    int nodes;        // node count (how many times was 'minimax' invoked)
+    int nonQNodes;    // non-quiesce node count
+    int moveGenNodes; // how many times was mListGenerate() called
+    int hashHitGood;  // hashtable hits that returned immediately.
+    int hashWroteNew; // how many times (in this ply) we wrote to a unique
+                      //  hash entry.  Used for UCI hashfull stats.
+    ThinkerStatsT();  // This struct can initialize itself.
+    void Clear();
+};
+
+inline ThinkerStatsT::ThinkerStatsT()
+{
+    Clear();
+}
+
+inline void ThinkerStatsT::Clear()
+{
+    memset(this, 0, sizeof(ThinkerStatsT));
+}
+
+#endif // THINKERTYPES_H
