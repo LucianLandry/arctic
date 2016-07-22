ea2c0add34af367d5431cac482e88bee63a9d0b2
Lucian Landry <lucian_b_landry@yahoo.com>
2016-07-22 08:42:54 -0500

    Add new engine configuration API + implementation
    Nothing actually uses it yet, though.

---
+++ Config.cpp
@@ -0,0 +1,251 @@
+//--------------------------------------------------------------------------
+//                  Config.cpp - engine option-setting API
+//                           -------------------
+//  copyright            : (C) 2016 by Lucian Landry
+//  email                : lucian_b_landry@yahoo.com
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Library General Public License as
+//   published by the Free Software Foundation; either version 2 of the
+//   License, or (at your option) any later version.
+//
+//--------------------------------------------------------------------------
+
+#include <algorithm> // std::find
+#include <assert.h>
+#include "Config.h"
+
+const char *Config::ErrorString(Config::Error error) const
+{
+    switch (error)
+    {
+        case Error::None:
+            return "No error";
+        case Error::NotFound:
+            return "Item not found";
+        case Error::WrongType:
+            return "Item has wrong type for this method";
+        case Error::InvalidValue:
+            return "Value out of range, or not a valid choice";
+        case Error::AlreadyExists:
+            return "Item already exists";
+        default:break;
+    }
+    assert(0); // All cases should be covered
+    return ""; // Should not reach here
+}
+
+void Config::CheckboxItem::SetValue(bool value)
+{
+    if (this->value != value)
+    {
+        this->value = value;
+        callback(*this);
+    }
+}
+
+Config::Error Config::SpinItem::SetValue(int value)
+{
+    if (value < min || value > max)
+        return Error::InvalidValue;
+    if (this->value != value)
+    {
+        this->value = value;
+        callback(*this);
+    }
+    return Error::None;
+}
+
+void Config::SpinItem::SetValueClamped(int value)
+{
+    value = std::max(value, min);
+    value = std::min(value, max);
+    
+    if (this->value != value)
+    {
+        this->value = value;
+        callback(*this);
+    }
+}
+
+Config::Error Config::ComboItem::SetValue(const std::string &value)
+{
+    if (std::find(choices.begin(), choices.end(), value) == choices.end())
+        return Error::InvalidValue;
+    if (this->value != value)
+    {
+        this->value = value;
+        callback(*this);
+    }
+    return Error::None;
+}
+
+void Config::StringItem::SetValue(const std::string &value)
+{
+    if (this->value != value)
+    {
+        this->value = value;
+        callback(*this);
+    }
+}
+
+void Config::ButtonItem::SetValue()
+{
+    callback(*this);
+}
+
+Config::~Config()
+{
+    // We should not leak the Items we copied.
+    for (auto &iter : itemMap)
+    {
+        delete iter.second;
+        iter.second = nullptr;
+    }
+}
+
+Config::Error Config::Register(const Item &item)
+{
+    if (itemMap.find(item.Name()) != itemMap.end())
+        return Error::AlreadyExists;
+
+    itemMap[item.Name()] = item.Clone();
+    return Error::None;
+}
+
+const Config::Item *Config::ItemAt(int idx) const
+{
+    for (const auto &iter : itemMap)
+    {
+        if (--idx < 0)
+            return iter.second;
+    }
+    return nullptr;
+}
+
+Config::Item *Config::ItemAt(const std::string &name) const
+{
+    auto iter = itemMap.find(name);
+    return iter != itemMap.end() ? iter->second : nullptr;
+}
+
+Config::Error Config::SetCheckbox(const std::string &name, bool value)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    CheckboxItem *cItem = dynamic_cast<CheckboxItem *>(item);
+
+    if (cItem == nullptr)
+        return Error::WrongType;
+
+    cItem->SetValue(value);
+    return Error::None;
+}
+
+Config::Error Config::SetSpin(const std::string &name, int value)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    SpinItem *sItem = dynamic_cast<SpinItem *>(item);
+
+    if (sItem == nullptr)
+        return Error::WrongType;
+
+    return sItem->SetValue(value);
+}
+
+Config::Error Config::SetSpinClamped(const std::string &name, int value)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    SpinItem *sItem = dynamic_cast<SpinItem *>(item);
+
+    if (sItem == nullptr)
+        return Error::WrongType;
+
+    sItem->SetValueClamped(value);
+    return Error::None;
+}
+
+Config::Error Config::SetCombo(const std::string &name, const std::string &value)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    ComboItem *cItem = dynamic_cast<ComboItem *>(item);
+
+    if (cItem == nullptr)
+        return Error::WrongType;
+
+    return cItem->SetValue(value);
+}
+
+Config::Error Config::SetButton(const std::string &name)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    ButtonItem *bItem = dynamic_cast<ButtonItem *>(item);
+
+    if (bItem == nullptr)
+        return Error::WrongType;
+
+    bItem->SetValue();
+    return Error::None;
+}
+
+Config::Error Config::SetString(const std::string &name, const std::string &value)
+{
+    Item *item = ItemAt(name);
+
+    if (item == nullptr)
+        return Error::NotFound;
+        
+    StringItem *sItem = dynamic_cast<StringItem *>(item);
+
+    if (sItem == nullptr)
+        return Error::WrongType;
+
+    sItem->SetValue(value);
+    return Error::None;
+}
+
+#if 0 // standalone test harness
+#include <iostream>
+void cbCallback(const Config::CheckboxItem &item, void *context)
+{
+    // const Config::CheckboxItem &cbItem =
+    //    dynamic_cast<const Config::CheckboxItem &>(item);
+    std::cout << "cbCallback: fire, context " << context << " value "
+              << std::boolalpha << item.Value() << '\n';
+}
+
+int main()
+{
+    Config cfg;
+    void *context = (void *) 0xdeadbeef;
+    cfg.Register(Config::CheckboxItem("canResign", "can machine resign",
+                                      false,
+                                      std::bind(cbCallback,
+                                                std::placeholders::_1,
+                                                context)));
+    cfg.SetCheckbox("canResign", true);
+    return 0;
+}
+#endif // standalone test harness
---
+++ Config.h
@@ -0,0 +1,315 @@
+//--------------------------------------------------------------------------
+//                   Config.h - engine option-setting API
+//                           -------------------
+//  copyright            : (C) 2016 by Lucian Landry
+//  email                : lucian_b_landry@yahoo.com
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Library General Public License as
+//   published by the Free Software Foundation; either version 2 of the
+//   License, or (at your option) any later version.
+//
+//--------------------------------------------------------------------------
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <assert.h>
+#include <functional>
+#include <map>
+#include <string>
+#include <vector>
+
+// To support UCI and Winboard equivalents, we need to support at least:
+// checkbox (true/false)
+// spin (integer w/in a range)
+// combo (string multiple choice)
+// button (a simple trigger)
+// string (a free-form string)
+
+// Pre-defined config items.
+#define kConfigMaxDepthSpin "limits/maxDepth" // 0 == 'no limit'
+#define kConfigMaxNodesSpin "limits/maxNodes" // 0 == 'no limit'
+#define kConfigRandomMovesCheckbox "randomMoves" // true iff engine should
+                                              //  randomize moves
+#define kConfigCanResignCheckbox   "canResign"   // true iff engine may resign
+
+// not used yet
+// #define kConfigHistoryWindowSpin "historyWindow"
+// #define kConfigTranpositionTableSizeSpin "limits/transpositionTableSize"
+
+class Config
+{
+public:
+    Config() = default;
+    Config(const Config &other) = default;
+    
+#if 0
+    enum class ItemType
+    {
+        Unknown, // No such item
+        Checkbox, // boolean checkbox
+        Spin,    // integer range
+        Combo,   // string multiple-choice
+        Button,  // button
+        String   // free-form string
+    };
+#endif
+    
+    ~Config(); // dtor
+
+    enum class Error
+    {
+        None,          // no error
+        NotFound,      // item not found
+        WrongType,     // tried to invoke method of wrong type for this item
+        InvalidValue,  // value out of range, or not a valid choice
+        AlreadyExists // Item already exists (Register() only)
+    };
+
+    const char *ErrorString(Error error) const;
+    
+    Error SetCheckbox(const std::string &name, bool value);
+    Error SetSpin(const std::string &name, int value);
+    Error SetSpinClamped(const std::string &name, int value);
+    Error SetCombo(const std::string &name, const std::string &value);
+    Error SetButton(const std::string &name);
+    Error SetString(const std::string &name, const std::string &value);
+
+    class Item;
+    Error Register(const Item &item);
+
+    // Retrieve an item by index.  This allows item discovery.  Returns item at
+    //  index 'idx' (or nullptr if no such item exists).
+    const Item *ItemAt(int idx) const;
+    
+    // Retrieve an item by name.  Returns nullptr if no such item exists.
+    Item *ItemAt(const std::string &name) const;
+
+    // Do a type_id check to figure this out.
+    // ItemType ItemType(const std::string &name) const;
+    class Item
+    {
+    protected:
+        Item(const std::string &name, const std::string &description);
+        Item(const Item &other) = default;
+    public:
+        virtual Item *Clone() const;
+        virtual ~Item() = default;
+        inline std::string Name() const;
+        inline std::string Description() const;
+    private:
+        std::string name;
+        std::string description;
+    };
+
+    class CheckboxItem final : public Item
+    {
+    public:
+        using CheckboxChangedFunc = std::function<void(const CheckboxItem &)>;
+        CheckboxItem(const std::string &name, const std::string &description,
+                     bool defaultValue, CheckboxChangedFunc callback);
+        CheckboxItem(const CheckboxItem &other) = default;
+        CheckboxItem *Clone() const override;
+        inline bool Value() const;
+        void SetValue(bool value);
+    private:
+        bool value = 0;
+        CheckboxChangedFunc callback;
+    };
+
+    class SpinItem final : public Item
+    {
+    public:
+        using SpinChangedFunc = std::function<void(const SpinItem &)>;
+        SpinItem(const std::string &name, const std::string &description,
+                 int min, int defaultValue, int max, SpinChangedFunc callback);
+        SpinItem(const SpinItem &other) = default;
+        SpinItem *Clone() const override;
+        inline int Value() const;
+        inline int Min() const;
+        inline int Max() const;
+        Error SetValue(int value);
+        void SetValueClamped(int value);
+    private:
+        int value = 0;
+        int min = 0, max = 0; // these are meant to be inclusive
+        SpinChangedFunc callback;
+    };
+
+    using ComboChoices = std::vector<std::string>;
+    class ComboItem final : public Item
+    {
+    public:
+        using ComboChangedFunc = std::function<void(const ComboItem &)>;
+        ComboItem(const std::string &name, const std::string &description,
+                  const std::string &defaultValue, const ComboChoices &choices,
+                  ComboChangedFunc callback);
+        ComboItem(const ComboItem &other) = default;
+        ComboItem *Clone() const override;
+        inline std::string Value() const;
+        inline ComboChoices Choices() const;
+        Error SetValue(const std::string &value);
+    private:
+        std::string value;
+        ComboChoices choices;
+        ComboChangedFunc callback;
+    };
+
+    class StringItem final : public Item
+    {
+    public:
+        using StringChangedFunc = std::function<void(const StringItem &)>;
+        StringItem(const std::string &name, const std::string &description,
+                   const std::string &defaultValue, StringChangedFunc callback);
+        StringItem(const StringItem &other) = default;
+        StringItem *Clone() const override;
+        inline std::string Value() const;
+        void SetValue(const std::string &value);
+    private:
+        std::string value;
+        StringChangedFunc callback;
+    };
+
+    class ButtonItem final : public Item
+    {
+    public:
+        using ButtonChangedFunc = std::function<void(const ButtonItem &)>;
+        ButtonItem(const std::string &name, const std::string &description,
+                   ButtonChangedFunc callback);
+        ButtonItem(const ButtonItem &other) = default;
+        ButtonItem *Clone() const override;
+        void SetValue(); // 'pushes' the button
+    private:
+        ButtonChangedFunc callback;
+    };
+
+private:
+    std::map<std::string, Item *> itemMap;
+};
+
+inline Config::Item::Item(const std::string &name,
+                          const std::string &description) :
+              name(name), description(description) {}
+
+inline Config::Item *Config::Item::Clone() const
+{
+    return new Item(*this);
+}
+
+inline std::string Config::Item::Name() const
+{
+    return name;
+}
+
+inline std::string Config::Item::Description() const
+{
+    return description;
+}
+
+inline Config::CheckboxItem::CheckboxItem(const std::string &name,
+                                          const std::string &description,
+                                          bool defaultValue,
+                                          CheckboxChangedFunc callback) :
+              Item(name, description), value(defaultValue), callback(callback)
+{
+}
+
+inline bool Config::CheckboxItem::Value() const
+{
+    return value;
+}
+
+inline Config::CheckboxItem *Config::CheckboxItem::Clone() const
+{
+    return new CheckboxItem(*this);
+}
+
+inline Config::SpinItem::SpinItem(const std::string &name,
+                                  const std::string &description,
+                                  int min, int defaultValue, int max,
+                                  SpinChangedFunc callback) :
+              Item(name, description), value(defaultValue), min(min), max(max),
+              callback(callback)
+{
+    assert(min <= max);
+}
+
+inline Config::SpinItem *Config::SpinItem::Clone() const
+{
+    return new SpinItem(*this);
+}
+
+inline int Config::SpinItem::Value() const
+{
+    return value;
+}
+
+inline int Config::SpinItem::Min() const
+{
+    return min;
+}
+
+inline int Config::SpinItem::Max() const
+{
+    return max;
+}
+
+inline Config::ComboItem::ComboItem(const std::string &name,
+                                    const std::string &description,
+                                    const std::string &defaultValue,
+                                    const ComboChoices &choices,
+                                    ComboChangedFunc callback) :
+              Item(name, description), value(defaultValue), choices(choices),
+              callback(callback)
+{
+    assert(std::find(choices.begin(), choices.end(), value) != choices.end());
+}
+
+inline Config::ComboItem *Config::ComboItem::Clone() const
+{
+    return new ComboItem(*this);
+}
+
+inline std::string Config::ComboItem::Value() const
+{
+    return value;
+}
+
+inline Config::ComboChoices Config::ComboItem::Choices() const
+{
+    return choices;
+}
+
+inline Config::StringItem::StringItem(const std::string &name,
+                                      const std::string &description,
+                                      const std::string &defaultValue,
+                                      StringChangedFunc callback) :
+              Item(name, description), value(defaultValue), callback(callback)
+{
+}
+
+inline Config::StringItem *Config::StringItem::Clone() const
+{
+    return new StringItem(*this);
+}
+
+inline std::string Config::StringItem::Value() const
+{
+    return value;
+}
+
+inline Config::ButtonItem::ButtonItem(const std::string &name,
+                                      const std::string &description,
+                                      ButtonChangedFunc callback) :
+              Item(name, description), callback(callback) {}
+
+inline Config::ButtonItem *Config::ButtonItem::Clone() const
+{
+    return new ButtonItem(*this);
+}
+
+#endif // CONFIG_H
